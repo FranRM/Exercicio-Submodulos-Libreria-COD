71d757dfec855dca299986967070a923fb4adba0
Femio23 <chiscazo.paco@gmail.com>
2018-01-25 09:53:43 +0100

    adicion dos cambios na clase main

--- src/exemplosmlibf/Exemplosmlibf.java
+++ src/exemplosmlibf/Exemplosmlibf.java
@@ -1,10 +1,13 @@
 package exemplosmlibf;
 import com.otros.milibreria.MisMetodos;
+import com.fran.mensaxes.Saudos;
 
 public class Exemplosmlibf {
 
     public static void main(String[] args) {
+        Saudos.entrada("o exemplo de submodulos.");
         System.out.println(MisMetodos.otroMetodo(25));
+        Saudos.saida();
         
     }
     
